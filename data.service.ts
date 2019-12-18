a7a581d0251de0f14c843ec92f06aa282c260771
Datha Sai Kalvakolanu <kbsndsai@gmail.com>
2019-12-18 11:34:53 +0530

    Create data.service.ts

---
+++ data.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {   
+  test = [];
+  x;
+  flag = true;
+
+  constructor() { }    
+  
+}
